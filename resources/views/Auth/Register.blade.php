24b95d8f77c81f58c2f99375f0a27be6aed60586
KrustFrost <chrisangeles30@gmail.com>
2021-12-17 14:27:58 +0800

    changes class css codes

--- resources/views/Auth/Register.blade.php
+++ resources/views/Auth/Register.blade.php
@@ -9,11 +9,11 @@
     <script src="{{ mix('js/app.js')}}"></script>
     <title>Admin/Backend Interface</title>
 </head>
-<body>
+<body class="bg-gray-100">
 <div class="flex flex-row">
 <div id="login" class="bg-gradient-to-r from-green-400 to-blue-500 flex flex-col w-6/12 p-5 relative" id="Login">
 <div class="mt-20">
-<p class="font-serif text-6xl p-10 font-mono flex justify-center break-word">Register here</p>
+<p class="font-serif text-6xl p font-mono flex justify-center p-2 break-word">Register here</p>
 <div class="flex justify-center">
         <div class="bg-gray-100 p-5 shadow-2xl rounded-b-lg w-8/12 flex flex-col">
         <form action="{{route('auth.register')}}" method="post" class="flex flex-col">
@@ -50,14 +50,14 @@
                     @enderror
                     <p><a href="/">Already have an account?</a></p>
                     <div class="flex justify-center">
-                    <button type="submit" name="submit" class="bg-black text-white w-1/5 p-2 mt-2 mb-2 rounded-lg shadow-md">Register</button>
+                    <button type="submit" name="submit" class="bg-black text-white p-2 mt-2 mb-2 rounded-lg shadow-md">Register</button>
                     </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-    <div class="bg-gray-100 w-screen h-screen m-auto">
+    <div class="">
     <div id="login_page_top_name" class="m-auto">
     <div id="page_name" ><img src="{{ asset('/storage/A.I_Logo.png') }}"  width="1000" class="m-auto"></div>
     </div>
