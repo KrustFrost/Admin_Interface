7e8e8b5251b57439ce703de05a8e5148772080a9
KrustFrost <chrisangeles30@gmail.com>
2021-12-17 16:37:08 +0800

    added xss middleware

--- routes/web.php
+++ routes/web.php
@@ -26,37 +26,37 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware(['guest']);
 
-Route::get('/login',[LoginController::class,'index'])->name('login');
-Route::post('/login',[LoginController::class,'store'])->name('login_active');
-Route::post('/logout',[LogoutController::class,'destroy'])->name('logout');
-Route::get('/Register',[RegisterController::class,'index'])->name('Register');
-Route::post('/Register',[RegisterController::class,'store'])->name('auth.register');
+Route::get('/login',[LoginController::class,'index'])->middleware(['XSS'])->name('login');
+Route::post('/login',[LoginController::class,'store'])->middleware(['XSS'])->name('login_active');
+Route::post('/logout',[LogoutController::class,'destroy'])->middleware(['XSS'])->name('logout');
+Route::get('/Register',[RegisterController::class,'index'])->middleware(['XSS'])->name('Register');
+Route::post('/Register',[RegisterController::class,'store'])->middleware(['XSS'])->name('auth.register');
 
-Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+Route::get('/dashboard',[DashboardController::class,'index'])->middleware(['XSS'])->name('dashboard');
 
-Route::get('/Profile_Management_System',[PMSController::class,'index'])->name('PMS');
+Route::get('/Profile_Management_System',[PMSController::class,'index'])->middleware(['XSS'])->name('PMS');
 
-Route::get('/About',[AboutController::class,'index'])->name('About');
-Route::get('/Team',[AboutController::class,'Team'])->name('Team');
+Route::get('/About',[AboutController::class,'index'])->middleware(['XSS'])->name('About');
+Route::get('/Team',[AboutController::class,'Team'])->middleware(['XSS'])->name('Team');
 
-Route::get('/Profile_Management_System/Profile/{id}',[PMSController::class,'Profile'])->name('PMS_profile');
-Route::post('/Profile_Management_System/Profile/{id}',[PMSController::class,'Profile_edit'])->name('PMS_profile_edit');
+Route::get('/Profile_Management_System/Profile/{id}',[PMSController::class,'Profile'])->middleware(['XSS'])->name('PMS_profile');
+Route::post('/Profile_Management_System/Profile/{id}',[PMSController::class,'Profile_edit'])->middleware(['XSS'])->name('PMS_profile_edit');
 
-Route::get('/Library_Management_System',[LMSController::class,'index'])->name('LMS');
-Route::get('/Library_Management_System/download/{id}',[LMSController::class,'index_download'])->name('LMS_download');
-Route::post('/Library_Management_System/download/{id}',[LMSController::class,'index_download'])->name('LMS_download');
-Route::post('/Library_Management_System',[LMSController::class,'index_delete']);
-Route::delete('/Library_Management_System/{id}',[LMSController::class,'index_delete']);
-Route::get('/Library_Management_System/create',[LMSController::class,'index_create'])->name('lms_create');
-Route::post('/Library_Management_System/create',[LMSController::class,'index_create_store'])->name('lms_create_post');
+Route::get('/Library_Management_System',[LMSController::class,'index'])->middleware(['XSS'])->name('LMS');
+Route::get('/Library_Management_System/download/{id}',[LMSController::class,'index_download'])->middleware(['XSS'])->name('LMS_download');
+Route::post('/Library_Management_System/download/{id}',[LMSController::class,'index_download'])->middleware(['XSS'])->name('LMS_download');
+Route::post('/Library_Management_System',[LMSController::class,'index_delete'])->middleware(['XSS']);
+Route::delete('/Library_Management_System/{id}',[LMSController::class,'index_delete'])->middleware(['XSS']);
+Route::get('/Library_Management_System/create',[LMSController::class,'index_create'])->middleware(['XSS'])->name('lms_create');
+Route::post('/Library_Management_System/create',[LMSController::class,'index_create_store'])->middleware(['XSS'])->name('lms_create_post');
 
-Route::get('/Calendar_System',[CSController::class,'index'])->name('CS');
+Route::get('/Calendar_System',[CSController::class,'index'])->middleware(['XSS'])->name('CS');
 
-Route::get('/User_Management_System',[UMSController::class,'index'])->name('UMS');
-Route::get('/User_Management_System/create',[UMSController::class,'createview'])->name('umscreateview');
-Route::post('/User_Management_System/create',[UMSController::class,'create'])->name('umscreate');
-Route::get('/User_Management_System/{id}',[UMSController::class,'updateview']);
-Route::post('/User_Management_System/{id}',[UMSController::class,'update'])->name('umsedit');
-Route::post('/User_Management_System',[UMSController::class,'delete']);
-Route::delete('/User_Management_System/{id}',[UMSController::class,'delete']);
+Route::get('/User_Management_System',[UMSController::class,'index'])->middleware(['XSS'])->name('UMS');
+Route::get('/User_Management_System/create',[UMSController::class,'createview'])->middleware(['XSS'])->name('umscreateview');
+Route::post('/User_Management_System/create',[UMSController::class,'create'])->middleware(['XSS'])->name('umscreate');
+Route::get('/User_Management_System/{id}',[UMSController::class,'updateview'])->middleware(['XSS']);
+Route::post('/User_Management_System/{id}',[UMSController::class,'update'])->middleware(['XSS'])->name('umsedit');
+Route::post('/User_Management_System',[UMSController::class,'delete'])->middleware(['XSS']);
+Route::delete('/User_Management_System/{id}',[UMSController::class,'delete'])->middleware(['XSS']);
 
